71d1b6d9d32a889daed23b8a63a535be037e9018
ilyascode <ilyasalghifari@gmail.com>
2024-04-23 14:59:51 +0700

    bcit

---
+++ bcit/application/views/biodata.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=<? ="biodata\assets\css\bootstrap.css">
+    <title>Welcome</title>
+</head>
+<center>
+<body>
+<nav>
+<div class="start">
+    <h1>Biodata Diri</h1>
+</div>
+</nav>
+
+<br><br>
+
+<header class="header">
+<figure class="text-center">
+    <img
+        src="ilyas.jpeg"
+        alt="profil saya"
+    >
+    <figcaption>IlyasAlghifari</figcaption>
+    <strong>(17210459)</strong>
+</figure>
+</header>
+
+<br><br><br>
+
+<main>
+<hr>
+<article class="content">
+    <p> Nama Lengkap: IlyasAlghifari</p>
+    <p> Kelas: 17.6A.26 </p>
+    <p>Alamat: Jl. Palmerah Barat</p>
+    <p>Hobby: Game </p>
+    <p>Cita - Cita: Manager</p>
+    <p>Pekerjaan: Freelance</p>
+</article>
+</main>
+
+<br><br>
+</body>
+</center>
+</html>
